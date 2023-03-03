85e1c675a75c3092f5d90332dd57143df7fe2a25
cod3f1re <cod4f1rex@gmail.com>
2023-03-03 11:10:15 -0600

    Se agrega un manager a producto

--- product/models.py
+++ product/models.py
@@ -1,23 +1,37 @@
 from django.db import models
-from django.shortcuts import reverse
 
 
 # Create your models here.
+class ProductManager(models.Manager):
+    def create_product(self, title, price, discount_price, description, slug, image, date_created, date_modified,
+                       date_inactive, active):
+        book = self.create(title=title, price=price, discount_price=discount_price, description=description,
+                           slug=slug, image=image, date_created=date_created, date_modified=date_modified,
+                           date_inactive=date_inactive,
+                           active=active)
+        # do something with the book
+        return book
+
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
     discount_price = models.FloatField(blank=True, null=True)
-    slug = models.SlugField()
     description = models.TextField()
+    slug = models.SlugField()
     image = models.ImageField()
     date_created = models.DateField()
     date_modified = models.DateField()
     date_inactive = models.DateField()
     active = models.BooleanField()
 
+    objects = ProductManager()
+
+    class Meta:
+        db_table = 'product'  # Le doy de nombre 'jugos' a nuestra tabla en la Base de Datos
+
     def __str__(self):
-        return "%s %s" % (self.title, self.price)
+        return "%s %s %s" % (self.title, self.price, self.image)
 
 
 class Category(models.Model):
