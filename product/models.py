a62e11971b923afbf3d85f2b88bae76fb2befa24
cod3f1re <cod4f1rex@gmail.com>
2023-03-02 19:02:51 -0600

    Se agrega modelo de productos y categorias

--- product/models.py
+++ product/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.shortcuts import reverse
+
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField()
+    date_created = models.DateField()
+    date_modified = models.DateField()
+    date_inactive = models.DateField()
+    active = models.BooleanField()
+
+    def __str__(self):
+        return "%s %s" % (self.title, self.price)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
